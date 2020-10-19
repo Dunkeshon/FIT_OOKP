be93808a6e32a405936a3a8ee639e44c5278f7ca
Dunkeshon <andrei.ioffe@gmail.com>
2020-10-19 12:50:18 +0300

    init commit

---
+++ eazyLab1/eazyLab1/statement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eazyLab1
+{
+    class Statement
+    {
+        public int WarehouseId { get; }
+        public float BalanceAtStart { get; }
+        public float Received { get; } // add to balance 
+        public float Issued { get; } // distract from balance 
+        public float BalanceInTheEnd { get; }
+
+        public Statement(int warehouseId, float balanceAtStart, float received, float issued)
+        {
+            WarehouseId = warehouseId;
+            BalanceAtStart = balanceAtStart;
+            Received = received;
+            Issued = issued;
+            BalanceInTheEnd = BalanceAtStart + Received - Issued;
+        }
+
+    }
+}
